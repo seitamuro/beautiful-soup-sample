efa91661bd1e9f1415c46ba4b260e128065b1520
seitamuro <seitamuro0210@gmail.com>
2024-09-21 22:14:04 +0900

    リポジトリ名と概要、各種日時のみを抽出するプログラムを作成

---
+++ extract-json.py
@@ -0,0 +1,18 @@
+# リポジトリーの名前、概要、作成日、更新日、プッシュ日を抽出する
+import json
+
+with open("aws-samples-repositories.json") as f:
+  repositories = json.load(f)
+
+data = []
+for repository in repositories:
+  data.append({
+    "name": repository["name"],
+    "description": repository["description"],
+    "created_at": repository["created_at"],
+    "updated_at": repository["updated_at"],
+    "pushed_at": repository["pushed_at"], 
+  })
+
+with open("aws-samples-repositories-extracted.json", "w") as f:
+  json.dump(data, f)
